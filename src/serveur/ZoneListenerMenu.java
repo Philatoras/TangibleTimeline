fc444b0806d75f5fafb9210547c0f60417d2a6e6
Lucas CLEMENT <lucas.clement@live.fr>
2019-02-14 20:14:44 +0100

    ajout ZoneListenerMenu (à implementer) + ajout zones ScenarioMenu

---
+++ src/serveur/ZoneListenerMenu.java
@@ -0,0 +1,16 @@
+package serveur;
+
+public class ZoneListenerMenu implements ZoneListener {
+
+	@Override
+	public void zoneEntered(ZoneEvent zoneEvent) {
+		//TODO
+	}
+
+	@Override
+	public void zoneExited(ZoneEvent zoneExited) {
+		//TODO
+
+	}
+
+}
