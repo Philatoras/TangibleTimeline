d1bff78eb6b2b1d3b579061aa98c7e5384d3fc3d
Gwendal Eynard <gwendaleynard@gmail.com>
2019-03-07 00:11:37 +0100

    Finalisation scénario quad

--- src/serveur/ScenarioQuad.java
+++ src/serveur/ScenarioQuad.java
@@ -14,6 +14,7 @@ public class ScenarioQuad implements ScenarioBloc{
 		PionBlock pionAuteur = new PionBlock("auteur","red","rond");
 		PionBlock pionPeintre = new PionBlock("peintre","yellow","rond");
 		
+		
 		ZoneBlock zonePleiade = new ZoneBlock("zonePleiade",110, 100,30,20,"Pléïade");
 		pionAuteur.addSortie(zonePleiade);
 		pionPeintre.addSortie(zonePleiade);
@@ -22,23 +23,63 @@ public class ScenarioQuad implements ScenarioBloc{
 		pionAuteur.addSortie(zoneBaroque);
 		pionPeintre.addSortie(zoneBaroque);
 		
+		ZoneBlock zoneLumieres = new ZoneBlock("zoneLumieres",170, 100,30,20,"Lumieres");
+		pionAuteur.addSortie(zoneLumieres);
+		pionPeintre.addSortie(zoneLumieres);
+		
+		ZoneBlock zoneRomantisme = new ZoneBlock("zoneRomantisme",200, 100,30,20,"Romantisme");
+		pionAuteur.addSortie(zoneRomantisme);
+		pionPeintre.addSortie(zoneRomantisme);
+		
+		ZoneBlock zoneSurrealisme = new ZoneBlock("zoneSurrealisme",200, 100,30,20,"Surrealisme");
+		pionAuteur.addSortie(zoneSurrealisme);
+		pionPeintre.addSortie(zoneSurrealisme);
+		
+		
 		ConditionBlock conditionAuteurPleiade = new ConditionBlock(Attributs.COULEUR, "red");
 		ConditionBlock conditionPeintrePleiade = new ConditionBlock(Attributs.COULEUR, "yellow");
 		ConditionBlock conditionAuteurBaroque = new ConditionBlock(Attributs.COULEUR, "red");
 		ConditionBlock conditionPeintreBaroque = new ConditionBlock(Attributs.COULEUR, "yellow");
+		ConditionBlock conditionAuteurSurrealisme = new ConditionBlock(Attributs.COULEUR, "red");
+		ConditionBlock conditionPeintreSurrealisme = new ConditionBlock(Attributs.COULEUR, "yellow");
+		ConditionBlock conditionAuteurLumieres = new ConditionBlock(Attributs.COULEUR, "red");
+		ConditionBlock conditionPeintreLumieres = new ConditionBlock(Attributs.COULEUR, "yellow");
+		ConditionBlock conditionAuteurRomantisme = new ConditionBlock(Attributs.COULEUR, "red");
+		ConditionBlock conditionPeintreRomantisme = new ConditionBlock(Attributs.COULEUR, "yellow");
+		
 		zonePleiade.addSortie(conditionAuteurPleiade);
 		zonePleiade.addSortie(conditionPeintrePleiade);
 		zoneBaroque.addSortie(conditionAuteurBaroque);
 		zoneBaroque.addSortie(conditionPeintreBaroque);
+		zoneRomantisme.addSortie(conditionAuteurRomantisme);
+		zoneRomantisme.addSortie(conditionPeintreRomantisme);
+		zoneSurrealisme.addSortie(conditionAuteurSurrealisme);
+		zoneSurrealisme.addSortie(conditionPeintreSurrealisme);
+		zoneLumieres.addSortie(conditionAuteurLumieres);
+		zoneLumieres.addSortie(conditionPeintreLumieres);
+		
 		
 		TextBlock textPeintreBaroque = new TextBlock("Rembrandt");
 		TextBlock textPeintrePleiade = new TextBlock("Leonard De Vinci");
 		TextBlock textAuteurBaroque = new TextBlock("Christofle de Beaujeu");
 		TextBlock textAuteurPleiade = new TextBlock("Rabelais");
+		TextBlock textPeintreRomantisme = new TextBlock("Théodore Géricault");
+		TextBlock textAuteurRomantisme = new TextBlock("Victor Hugo");
+		TextBlock textPeintreLumiere = new TextBlock("Jean-Antoine Watteau");
+		TextBlock textAuteurLumiere = new TextBlock("Voltaire");
+		TextBlock textPeintreSurrealisme = new TextBlock("Salvador Dali");
+		TextBlock textAuteurSurrealisme = new TextBlock("André Breton");
+		
 		conditionAuteurBaroque.addSortie(textAuteurBaroque);
 		conditionAuteurPleiade.addSortie(textAuteurPleiade);
 		conditionPeintreBaroque.addSortie(textPeintreBaroque);
 		conditionPeintrePleiade.addSortie(textPeintrePleiade);
+		conditionAuteurLumieres.addSortie(textAuteurLumiere);
+		conditionAuteurRomantisme.addSortie(textAuteurRomantisme);
+		conditionAuteurSurrealisme.addSortie(textAuteurSurrealisme);
+		conditionPeintreRomantisme.addSortie(textPeintreRomantisme);
+		conditionPeintreSurrealisme.addSortie(textPeintreSurrealisme);
+		conditionPeintreLumieres.addSortie(textPeintreLumiere);
 		
 		TTSBlock tts = new TTSBlock();
 		textPeintreBaroque.addSortie(tts);
@@ -48,6 +89,10 @@ public class ScenarioQuad implements ScenarioBloc{
 		
 		zoneManager.addZone(zoneBaroque);
 		zoneManager.addZone(zonePleiade);
+		zoneManager.addZone(zoneSurrealisme);
+		zoneManager.addZone(zoneRomantisme);
+		zoneManager.addZone(zoneLumieres);
+		
 		zoneManager.addPion(pionPeintre);
 		zoneManager.addPion(pionAuteur);
 	}
