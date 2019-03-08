ca65ca753a9ca11727a3bc774d107494c7e4bad1
Xavier <Xavier@172.17.48.62>
2019-03-08 11:31:28 +0100

    Xav

--- src/simulation/TypePionFrise.java
+++ src/simulation/TypePionFrise.java
@@ -4,8 +4,10 @@ import java.awt.Color;
 
 public enum TypePionFrise 
 {
-	PIONF("PionF", "pionf", "blue", new Color(164,164,164)), MENU("Menu", "menu", "red", new Color(250,0,0)),
-	PIONJ("PionJ", "pionj", "yellow", new Color(250,250,0)),PIONR("PionR", "pionr", "red", new Color(250,0,0));
+	PIONF("PionF", "pionf", "blue", new Color(164,164,164)),
+	MENU("Menu", "menu", "red", new Color(250,0,0)),
+	PIONJ("PionJ", "pionj", "yellow", new Color(250,250,0)),
+	PIONR("PionR", "pionr", "red", new Color(250,0,0));
 
 	
 	private String libelle, nom, couleurJeton;
