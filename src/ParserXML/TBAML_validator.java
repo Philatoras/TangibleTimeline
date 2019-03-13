11896c293faf9e5f8701ac3f4d498e75edde7b18
Ludovic <Ludovic@172.17.55.46>
2019-03-13 09:08:26 +0100

    validateur xsd

--- src/ParserXML/TBAML_validator.java
+++ src/ParserXML/TBAML_validator.java
@@ -1,5 +1,22 @@
 package ParserXML;
 
+import java.io.File;
+
 public class TBAML_validator {
+	private File myTBAMLfile;
+	private File schemaFileTbaml;
+	
+	public TBAML_validator(File tbamlFile) {
+		this.myTBAMLfile=tbamlFile;
+		File schemaFileTbaml;
+		
+	}
+	
+	public boolean validateTest() {
+		schemaFileTbaml = new File("/tba.xsd"); 
+		//en cours...   
+		return false;
+	}
+
 
 }
