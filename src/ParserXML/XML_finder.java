ac04446d367637e5574ba7d520a5871acf0ec84d
Ludovic <Ludovic@172.17.55.46>
2019-03-13 08:47:42 +0100

    le gestionnaire de fichier n'autorise l'ouverture que des fichiers tbaml (ou xml)

--- src/ParserXML/XML_finder.java
+++ src/ParserXML/XML_finder.java
@@ -1,16 +1,23 @@
 package ParserXML;
 
-	import java.io.File;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 
-	import javax.swing.JFileChooser;
-	import javax.swing.filechooser.FileSystemView;
-
+//Classe permetant l'ouverture d'un explorateur de fichier afin de selectioner un fichier .TBAML 
+//La fonction fileChooser retourne le fichier choisi
+	
 	public class XML_finder {
-		
 		
 		public File fileChooser()  {
 			File selectedFile = null;
 			JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+			jfc.setDialogTitle("Ouverture d'un fichier tbaml ");
+			jfc.setAcceptAllFileFilterUsed(false);
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("fichier tbaml ou xml", ".tbaml", ".xml");
+			jfc.addChoosableFileFilter(filter);
+
 			int returnValue = jfc.showOpenDialog(null);
 
 			if (returnValue == JFileChooser.APPROVE_OPTION) {
@@ -21,5 +28,10 @@ package ParserXML;
 			return selectedFile;
 
 		}
+		
+		public static void main(String[] args) {
+			XML_finder f=new XML_finder();
+			f.fileChooser();
+		}	
 
 	}
